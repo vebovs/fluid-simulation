2365c61ae05d50672777b781e2fef7857d29bbd8
vebovs <vebovs@hotmail.no>
2021-03-27 16:22:38 +0100

    first commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class Main {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        frame.setTitle("Demo");
+        frame.setSize(450, 475);
+        frame.setLocation(120, 60);
+        frame.setVisible(true);
+
+    }
+}
