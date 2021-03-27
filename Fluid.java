9ef5cc3f3470addc693235a10e1b2e387753fc18
vebovs <vebovs@hotmail.no>
2021-03-27 18:16:38 +0100

    step function

--- Fluid.java
+++ Fluid.java
@@ -37,6 +37,36 @@ class Fluid {
         this.Vy0 = new double[N * N];
     
     }
+
+    public void Step() {
+        int N = this.N;
+
+        double visc = this.visc;
+        double diff = this.diff;
+        double dt = this.dt;
+
+        double[] Vx = this.Vx;
+        double[] Vy = this.Vy;
+
+        double[] Vx0 = this.Vx0;
+        double[] Vy0 = this.Vy0;
+
+        double[] s = this.s;
+        double[] density = this.density;
+        
+        Utils.diffuse(1, Vx0, Vx, visc, dt, 4, N);
+        Utils.diffuse(2, Vy0, Vy, visc, dt, 4, N);
+        
+        Utils.project(Vx0, Vy0, Vx, Vy, 4, N);
+        
+        Utils.advect(1, Vx, Vx0, Vx0, Vy0, dt, N);
+        Utils.advect(2, Vy, Vy0, Vx0, Vy0, dt, N);
+        
+        Utils.project(Vx, Vy, Vx0, Vy0, 4, N);
+        
+        Utils.diffuse(0, s, density, diff, dt, 4, N);
+        Utils.advect(0, density, s, Vx, Vy, dt, N);
+    }
     
     private void addDensity(int x, int y, double amount) {
         this.density[Utils.IX(x, y, this.N)] += amount;
--- main.java
+++ main.java
@@ -2,8 +2,10 @@
 class Main {
     public static void main(String[] args) {
         
-        App app = new App();
-        app.Run();
+        //App app = new App();
+        //app.Run();
+
+        Fluid fluid = new Fluid(0.1, 0, 0);
 
     }
 }
