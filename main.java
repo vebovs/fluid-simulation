b0b5469eb70ee1ef61d70334c4b0e3ce7a629ef7
vebovs <vebovs@hotmail.no>
2021-03-27 22:28:04 +0100

    set main to default

--- main.java
+++ main.java
@@ -2,10 +2,8 @@
 class Main {
     public static void main(String[] args) {
         
-        //App app = new App();
-        //app.Run();
-
-        Fluid fluid = new Fluid(0.1, 0, 0);
+        App app = new App();
+        app.Run();
 
     }
 }
