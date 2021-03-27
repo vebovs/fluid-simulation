af85e80fdef89f355bf1de6aa0ad5000d93255c5
vebovs <vebovs@hotmail.no>
2021-03-27 16:44:51 +0100

    fixed graphics naming conflict

---
+++ Draw.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+
+class Draw extends JPanel {
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.BLACK);
+    }
+    
+}
