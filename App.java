57a0de7a67a53ddeb9a8120e59b9b04dd0dde502
vebovs <vebovs@hotmail.no>
2021-04-01 21:31:47 +0200

    fixed drawing not fitting window size

--- App.java
+++ App.java
@@ -1,19 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 class App extends JFrame {
     
     private Draw draw;
     private int size = 500;
-    private int scale = 3;
+    private int scale = 4;
 
     public App() {
+        this.setTitle("Fluid simulation");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setTitle("Fluid simulation");
         this.setLocation(1000, 450);
-        this.setSize(size + 16, size + 39);
         draw = new Draw(this.size, this.scale);
+        draw.setPreferredSize(new Dimension(size, size));
         this.add(draw);
+        this.pack();
     }
 
     public void Run() {
